3c9a68d4ec6d9f658f63335beaf76982a2fe6657
Jens Luedicke <jens.luedicke@gmail.com>
2010-12-28 19:01:10 +0100

    Use Module::Build instead of ExtUtils::MakeMaker.

--- lib/File/DirWalk.pm
+++ lib/File/DirWalk.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2005-2006 Jens Luedicke <jensl@cpan.org>. All rights reserved.
+# Copyright (c) 2005-2010 Jens Luedicke <jensl@cpan.org>. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 
